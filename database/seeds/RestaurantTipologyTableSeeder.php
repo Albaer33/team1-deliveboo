9a18fee1daf37857059f4b4c2e14cda257d127a5
RomeoAlex <romeo.alessandro17@gmail.com>
2022-03-30 17:18:20 +0200

    seeder per la tabella pivot ristoranti_tipologie

--- database/seeds/RestaurantTipologyTableSeeder.php
+++ database/seeds/RestaurantTipologyTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class RestaurantTipologyTableSeeder extends Seeder
 {
     /**
@@ -11,15 +11,21 @@ class RestaurantTipologyTableSeeder extends Seeder
      */
     public function run()
     {
-        $restaurantTipology = [
-                [
-                'restaurant_id' => '1',
-                'tipology_id' => '1'
-                ],
-                [
-                'restaurant_id' => '2',
-                'tipology_id' => '1'
-                ],
-        ];
+        DB::table('role_user')->insert(array(
+            array('restaurant_id' => 1, 'tipology_id' => 1),
+            array('restaurant_id' => 2, 'tipology_id' => 4),
+            array('restaurant_id' => 3, 'tipology_id' => 6),
+            array('restaurant_id' => 4, 'tipology_id' => 6),
+            array('restaurant_id' => 5, 'tipology_id' => 4),
+            array('restaurant_id' => 6, 'tipology_id' => 3),
+            array('restaurant_id' => 7, 'tipology_id' => 1),
+            array('restaurant_id' => 8, 'tipology_id' => 1),
+            array('restaurant_id' => 9, 'tipology_id' => 1),
+            array('restaurant_id' => 10, 'tipology_id' => 3),
+            array('restaurant_id' => 11, 'tipology_id' => 12),
+            array('restaurant_id' => 12, 'tipology_id' => 13),
+            
+        ));
+       
     }
 }
