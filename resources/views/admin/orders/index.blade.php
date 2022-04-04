4a8446593917891f805f300e0c3bbcfab152d7e7
RomeoAlex <romeo.alessandro17@gmail.com>
2022-04-04 19:29:29 +0200

    correzione tabella e stili

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -5,26 +5,23 @@
         <h1 class="title-c">Storico Ordini</h1>
 
         <div class="container">
-            <div class="row row-cols-3">
-                @foreach ($orders as $order)
-                    <div class="card">
-
-                        
-                            <table class="table">
-                                <thead>
-                                  <tr>
-                                    <th scope="col">Nome:</th>
-                                    <th scope="col">Indirizzo:</th>
-                                    <th scope="col">Dettagli:</th>
-                                    <th scope="col">Codice Transazione:</th>
-                                    <th scope="col">Totale: €</th>
-                                    <th scope="col">Email:</th>
-                                    <th scope="col">Telefono:</th>
-                                    <th scope="col">Pagato:</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
+            <table class="table">
+                <thead class="table-primary table table-striped">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome:</th>
+                        <th scope="col">Indirizzo:</th>
+                        <th scope="col">Dettagli:</th>
+                        <th scope="col">Codice Transazione:</th>
+                        <th scope="col">Totale: €</th>
+                        <th scope="col">Email:</th>
+                        <th scope="col">Telefono:</th>
+                        <th scope="col">Pagato:</th>
+                    </tr>
+                </thead>
+                            <tbody>
+                                <tr>
+                        @foreach ($orders as $order)
                                     <th scope="row">{{$order->id}}</th>
                                     <td>{{$order->nome}}</td>
                                     <td>{{$order->indirizzo}}</td>
@@ -39,20 +36,10 @@
                                         <td class="non-pagato">No</td>
                                     @endif
                                     <td><a href="{{route('admin.orders.show',['order' => $order->id])}}" class="btn btn_ms_primary_color">Dettagli</a></td>
+                        @endforeach
                                   </tr>
                                 </tbody>
-                              </table>
-                            
-
-                            
-
-                            
-
-                        
-                    </div>
-                @endforeach
-            </div>
-            
+                </table>
         </div>
     </section>
 @endsection
